1f10019ab9687c31fa39241267aa5ceb1b36cce0
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-03-31 15:24:10 +0200

    test.sh: run unit tests and tests in example crate

--- test.sh
+++ test.sh
@@ -1,6 +1,17 @@
 #!/bin/sh -ve
 export RUST_BACKTRACE=full
 
-cargo build
-cargo test
+cargo clean
+cargo update
+
+# try to generate doc
 cargo doc
+
+# run unit tests
+cargo test
+
+cargo clean
+
+cd example
+./test.sh
+cd ..
